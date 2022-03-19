b5a36cef23d72daa25990964b85272cbdeb40648
Kevin Carrington <Kevington99@gmail.com>
2022-03-18 17:14:23 -0700

    completed first 5 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+    let longWord = '';
+
+    if (num < 0) return 'ERROR';
+
+    for (let i = 0; i < num; i++){
+        longWord += string;
+    }  
+    return longWord;
 };
 
 // Do not edit below this line
