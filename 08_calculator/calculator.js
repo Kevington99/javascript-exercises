071adf1bccfec11110208220e77432e98d9ecd56
Kevin Carrington <Kevington99@gmail.com>
2022-03-24 21:24:31 -0700

    Completed Javascript Fundamentals 5 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return (a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a - b)
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	const total = nums.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    const mult = nums.reduce((previousValue, currentValue) => previousValue * currentValue);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1) {
+    return 1;
+  } else { 
+	return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let newStr = str.toLowerCase().replace(/[^-a-z0-9]/ig,'');
+
+    return ((newStr.split('').reverse().join('')) === newStr); 
 };
-
+  
 // Do not edit below this line
 module.exports = palindromes;
